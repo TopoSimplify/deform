c96a5cab020291367f33bfa41181189337e3b214
Titus Tienaah <titus.intdxdt@gmail.com>
2017-11-03 22:01:10 -0300

    cleanup

--- contig.go
+++ contig.go
@@ -8,24 +8,18 @@ import (
 func contiguousCandidates(a, b *node.Node) (*node.Node , *node.Node ){
 	//var selection = make([]*node.Node, 0)
 	// compute sidedness relation between contiguous hulls to avoid hull flip
-	//var hulls = []*node.Node{a, b}
-	//sort.Sort(node.Nodes(hulls))
-	// future should not affect the past
-	//ha, hb := hulls[0], hulls[1]
 
 	// all hulls that are simple should be collapsible
 	// if not collapsible -- add to selection for deformation
 	// to reach collapsibility
 	var  sa, sb *node.Node
 	//& the present should not affect the future
-	bln := a.Collapsible(b)
-	if !bln {
+	if !a.Collapsible(b) {
 		sa =  a
 	}
 
 	// future should not affect the present
-	bln = b.Collapsible(a)
-	if !bln {
+	if !b.Collapsible(a) {
 		sb =  b
 	}
 	return sa, sb
